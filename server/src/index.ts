3282a69afea92cc221c78d2d0224b92bf3b31b3b
Sam281055 <sl281055@gmail.com>
2024-10-03 15:07:24 -0400

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,8 @@ import morgan from "morgan"
 import dashboardRoutes from "./routes/dashboardRoutes"
 import productRoutes from "./routes/productRoutes"
 import userRoutes from "./routes/userRoutes"
+import expenseRoutes from "./routes/expenseRoutes"
+
 // Configurations
 dotenv.config();
 const app = express();
@@ -23,8 +25,9 @@ app.use(cors());
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
 // SERVER
-const port = process.env.PORT || 3001;
-app.listen(port, ()=>{
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0",()=>{
     console.log(`Server running on port ${port}`);
 })
